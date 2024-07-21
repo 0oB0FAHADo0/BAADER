9d45db5f298903fd0a709b076730570e44faeb66
0oB0FAHADo0 <mohammad.620305@gmail.com>
2024-07-21 11:02:43 +0300

    connect with database

---
+++ Bader/Models/BaaderContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Bader.Models
+{
+    public class BaaderContext : DbContext
+    {
+        public BaaderContext()
+        {
+
+        }
+        public BaaderContext(DbContextOptions<BaaderContext> options) : base(options)
+        {
+        }
+        public DbSet<tblUsers> tblUsers { get; set; }
+        public DbSet<tblRoles> tblRoles { get; set; }
+        public DbSet<tblColleges> tblColleges { get; set; }
+        public DbSet<tblPermissions> tblPermissions { get; set; }
+        public DbSet<tblLevels> tblLevels { get; set; }
+        public DbSet<tblCourses> tblCourses { get; set; }
+        public DbSet<tblContents> tblContents { get; set; }
+        public DbSet<tblSessionsState> tblSessionsState { get; set; }
+        public DbSet<tblSessions> tblSessions { get; set; }
+        public DbSet<tblRegistrationsState> tblRegistrationsState { get; set; }
+        public DbSet<tblRegistrations> tblRegistrations { get; set; }
+        public DbSet<tblPermissionsLogs> tblPermissionsLogs { get; set; }
+        public DbSet<tblCoursesLogs> tblCoursesLogs { get; set; }
+        public DbSet<tblContentsLogs> tblContentsLogs { get; set; }
+        public DbSet<tblSessionsLogs> tblSessionsLogs { get; set; }
+        public DbSet<tblRegistrationsLogs> tblRegistrationsLogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+           .AddJsonFile("appsettings.json")
+           .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DBCS"));
+        }
+    }
+}
--- Bader/Program.cs
+++ Bader/Program.cs
@@ -1,11 +1,46 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Bader;
+using System.Configuration;
+using Bader.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSession();
+//For Connection To  Data Base
+builder.Services.AddDbContext<BaaderContext>(options =>
+                 options.UseSqlServer(builder.Configuration.GetConnectionString("DBCS")));
+// For Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.AccessDeniedPath = "/Home/Error";
+                options.LoginPath = "/account/login";
+                options.ExpireTimeSpan = TimeSpan.FromDays(1);
+                //options.LoginPath = "/accounts/ErrorNotLoggedIn";
+                //options.LogoutPath = "account/logout";
+            });
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBeAdmin", p => p.RequireAuthenticatedUser().RequireRole("Admin"));
+});
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.Strict;
+    options.HttpOnly = HttpOnlyPolicy.Always;
+    options.Secure = CookieSecurePolicy.None;
+});
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -13,15 +48,30 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseCookiePolicy();
+app.UseAuthentication();
 app.UseRouting();
-
 app.UseAuthorization();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllerRoute(
+//        name: "Admin",
+//        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+//    endpoints.MapControllerRoute(
+//        name: "default",
+//        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//    endpoints.MapRazorPages();
+//});
 
 app.Run();
