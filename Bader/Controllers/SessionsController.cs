9b16233247b3fee94754e850d2d660f3fd9d8870
Abdulmohsen <Abdulmohsen@DESKTOP-3NGB8DJ>
2024-08-12 12:49:29 +0300

    add session

---
+++ Bader/Controllers/SessionsController.cs
@@ -0,0 +1,167 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bader.Models;
+using Bader.Domain;
+using Bader.ViewModels;
+
+namespace Bader.Controllers
+{
+    public class SessionsController : Controller
+    {
+        private readonly SessionsDomain _SessionsDomain;
+
+        public SessionsController(SessionsDomain context)
+        {
+            _SessionsDomain = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _SessionsDomain.GetSessions());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(Guid Id)
+        {
+            SessionsViewModel session = await _SessionsDomain.GetSessionsById(Id);
+            session.IsDeleted = true;
+
+            int check = await _SessionsDomain.UpdateSessions(session);
+
+            if (check == 1)
+            {
+                ViewData["Successful"] = "تم الحذف بنجاح";
+
+            }
+            else
+            {
+
+                ViewData["Falied"] = "حدث خطأ أثناء معالجتك طلبك الرجاء المحاولة في وقت لاحق";
+
+            }
+
+
+            return View(await _SessionsDomain.GetSessions());
+
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var Session = await _SessionsDomain.getFromSessionsState();
+            ViewBag.SessionList = new SelectList(Session, "Id", "StateAr");
+
+            var Course = await _SessionsDomain.getFromtblCourses();
+            ViewBag.CourseList = new SelectList(Course, "Id", "CourseNameAr");
+            return View();
+        }
+
+        // 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SessionsViewModel session)
+        {
+
+
+
+            try
+            {
+              //  if (session.RegEndDate<session.RegStartDate) {
+                var SessionState = await _SessionsDomain.getFromSessionsState();
+                ViewBag.SessionList = new SelectList(SessionState, "Id", "StateAr");
+
+                var Course = await _SessionsDomain.getFromtblCourses();
+
+                if (ModelState.IsValid)
+                {
+                    int check = await _SessionsDomain.AddSessions(session); ;
+                    if (check == 1)
+                    {
+                        ViewData["Successful"] = "تمت الإضافة بنجاح";
+
+                    }
+                    else
+                    {
+                        ViewData["Falied"] = "حدث خطأ أثناء معالجتك طلبك الرجاء المحاولة في وقت لاحق";
+
+                    }
+                }
+            }
+            catch
+            {
+                ViewData["Falied"] = "حدث خطأ أثناء معالجتك طلبك الرجاء المحاولة في وقت لاحق";
+
+            }
+           
+             return View();
+
+        }
+
+        //ed
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var session = await _SessionsDomain.GetSessionsById(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            var Session = await _SessionsDomain.getFromSessionsState();
+            ViewBag.SessionList = new SelectList(Session, "Id", "StateAr");
+
+            var Course = await _SessionsDomain.getFromtblCourses();
+            ViewBag.CourseList = new SelectList(Course, "Id", "CourseNameAr");
+            return View(session);
+
+
+        }
+
+        // POST:Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Edit(SessionsViewModel session)
+        {
+            try {
+
+                if (ModelState.IsValid)
+                  {
+                    int check = await _SessionsDomain.UpdateSessions(session);
+                    if (check == 1)
+                    {
+                        ViewData["Successful"] = "تم التعديل بنجاح";
+
+                    }
+                    else
+                    {
+                        ViewData["Falied"] = "حدث خطأ أثناء معالجتك طلبك الرجاء المحاولة في وقت لاحق";
+
+                    }
+                   }
+                }
+            catch {
+                ViewData["Falied"] = "حدث خطأ أثناء معالجتك طلبك الرجاء المحاولة في وقت لاحق";
+
+            }
+            return View();
+            // return RedirectToAction(nameof(Index));
+            //return RedirectToAction("Index");
+
+        }
+         
+        }
+
+       
+       
+    }
+
+
+
---
+++ Bader/ViewModels/SessionsViewModel.cs
@@ -0,0 +1,113 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using Bader.Models;
+
+namespace Bader.ViewModels
+{
+    public class SessionsViewModel
+    {
+
+        //star
+       // public int? Id { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [DisplayName("معرف حالة الجلسة")]
+        //public tblSessionsState SessionState { get; set; }
+        public int SessionStateId { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [StringLength(100)]
+        [DisplayName("إسم الجلسة بالعربي")]
+        public string SessionNameAr { get; set; }
+        //end
+
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [StringLength(100)]
+        [DisplayName("إسم الجلسة بالانجليزي")]
+        public string SessionNameEn { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [DisplayName("معرف الدورة")]
+        //  public tblCourses Course { get; set; }
+        public int CourseId { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [StringLength(100)]
+        [DisplayName("العنوان بالعربي")]
+        public string TitleAr { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [StringLength(100)]
+        [DisplayName("العنوان بالانجليزي")]
+        public string TitleEn { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [StringLength(200)]
+        [DisplayName("الروابط")]
+        public string Links { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [DisplayName("عدد الطلاب")]
+        public int NumOfStudents { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [DisplayName("تاريخ الجلسة")]
+        [DataType(DataType.DateTime)]
+
+        public DateTime SessionDate { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [DisplayName("تاريخ نهاية التسجيل")]
+        [DataType(DataType.DateTime)]
+
+        public DateTime RegEndDate { get; set; }
+        //end
+
+        //star
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [DisplayName("تاريخ بداية التسجيل")]
+        [DataType(DataType.DateTime)]
+        public DateTime RegStartDate { get; set; }
+        //end
+
+        //star
+
+        [DisplayName("حالة الجلسة")]
+        public string StateAr { get; set; }
+        //end
+        [DisplayName("اسم المقرر")]
+        public string CourseNameAr { get; set; }
+        //end
+
+        //star
+
+        [DisplayName("معرف (Guid)")]
+        public Guid GUID { get; set; }
+        //end
+
+        public bool IsDeleted { get; set; }
+    }
+}
