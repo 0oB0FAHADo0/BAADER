689b465a037f4f3824efcf8cdcf59805fd871a39
rakanmou <raakx77@gmail.com>
2024-08-13 13:56:55 +0300

    level add

---
+++ Bader/Domain/LevelDomain.cs
@@ -0,0 +1,118 @@
+﻿using Bader.Models;
+using Bader.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace Bader.Domain
+{
+    public class LevelDomain
+    {
+        private readonly BaaderContext _context;
+
+        public LevelDomain(BaaderContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<LevelViewModel>> GetLevels()
+        {
+
+            return await _context.tblLevels.Where(u => u.IsDeleted == false).Select(x => new LevelViewModel
+            {
+
+                LevelNameAr = x.LevelNameAr,
+                LevelNameEn = x.LevelNameEn,
+                LevelNum = x.LevelNum,
+                GUID = x.GUID,
+
+            }).ToListAsync();
+
+        }
+
+        public async Task<int> AddLevel(LevelViewModel Level)
+        {
+            try
+            {
+                tblLevels Levelx = new tblLevels();
+
+
+                Levelx.LevelNameAr = Level.LevelNameAr;
+                Levelx.LevelNameEn = Level.LevelNameEn;
+                Levelx.LevelNum = Level.LevelNum;
+                Levelx.GUID = Guid.NewGuid();
+                Levelx.IsDeleted = false;
+
+                _context.tblLevels.Add(Levelx);
+                int check = await _context.SaveChangesAsync();
+                return check;
+            }
+            catch
+            {
+                return 0;
+            }
+
+
+        }
+
+        public async Task<LevelViewModel> GetLevelbyId(Guid gudi)
+        {
+            var Level = _context.tblLevels.AsNoTracking().FirstOrDefault(tblLevels => tblLevels.GUID == gudi);
+
+
+            LevelViewModel Levelx = new LevelViewModel();
+            Levelx.LevelNameAr = Level.LevelNameAr;
+            Levelx.LevelNameEn = Level.LevelNameEn;
+            Levelx.LevelNum = Level.LevelNum;
+            Levelx.GUID = Level.GUID;
+
+            return Levelx;
+        }
+        public int GetLevelIdByGuid(Guid gudi)
+        {
+
+            var Level = _context.tblLevels.AsNoTracking().FirstOrDefault(tblLevels => tblLevels.GUID == gudi);
+
+            return Level.Id;
+
+        }
+
+        public async Task<int> UpdateLevel(LevelViewModel Level)
+        {
+
+            try
+            {
+                tblLevels Levelx = new tblLevels();
+
+
+                Levelx.Id = GetLevelIdByGuid(Level.GUID);
+
+                Levelx.GUID = Level.GUID;
+
+
+                Levelx.LevelNameAr = Level.LevelNameAr;
+                Levelx.LevelNameEn = Level.LevelNameEn;
+                Levelx.LevelNum = Level.LevelNum;
+                Levelx.IsDeleted = Level.IsDeleted;
+
+                _context.tblLevels.Update(Levelx);
+                int check = await _context.SaveChangesAsync();
+                return check;
+            }
+            catch
+            {
+                return 0;
+            }
+
+        }
+        public async Task<bool> LevelNumExists(int LevelNum, Guid guid)
+        {
+
+            //var CheckExist = _context.tblLevels.SingleOrDefault(n => n.BuildingNum == BuildingNum);
+            return await _context.tblLevels.Where(u => u.GUID != guid).Where(u => u.IsDeleted == false).AnyAsync(u => u.LevelNum == LevelNum);
+
+        }
+
+    }
+}
---
+++ Bader/ViewModels/LevelViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bader.ViewModels
+{
+    public class LevelViewModel
+    {
+
+        [Required(ErrorMessage = "هذا الحقل إجباري")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("المعرف")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [StringLength(100)]
+        [DisplayName("اسم المستوى بالعربي")]
+        public string LevelNameAr { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [StringLength(100)]
+        [DisplayName("اسم المستوى بالانجليزي")]
+        public string LevelNameEn { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [Range(1, 100, ErrorMessage = "رقم المستوى يجب أن يكون بين 1 و 100")]
+        [DisplayName("رقم المستوي")]
+        public int LevelNum { get; set; }
+
+        public Guid GUID { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
