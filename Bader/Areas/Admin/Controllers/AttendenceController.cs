8723741052f7956773749af89099a6902ed64684
alooooy211 <alooooy21@gmail.com>
2024-09-08 13:41:14 +0300

    slam

--- Bader/Areas/Admin/Controllers/AttendenceController.cs
+++ Bader/Areas/Admin/Controllers/AttendenceController.cs
@@ -2,8 +2,10 @@
 
 namespace Bader.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AttendenceController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
