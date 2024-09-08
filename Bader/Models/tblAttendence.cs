1959a46588425e5bc08feb25f6c6cfe5306633d6
علي بن محمد الغزال <219023289@student.kfu.edu.sa>
2024-09-08 11:55:08 +0300

    attend

--- Bader/Models/tblAttendence.cs
+++ Bader/Models/tblAttendence.cs
@@ -3,6 +3,18 @@
     public class tblAttendence// mohammmed alsubai ( viewModel,controler,view, domain) (add , update, delete)
     {
         public int Id { get; set; }
-        
+        public tblSessions Sesiion { get; set; }
+
+        public int SessionId { get; set; }
+
+        public string UserName { get; set; }
+
+        public int SessionDate { get; set; }
+
+
+        public string IsAttend { get; set; }
+
+
+
     }
 }
