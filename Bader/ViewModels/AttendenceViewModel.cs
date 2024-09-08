6622192f9bce555948811ea890ea1dec39b38a43
alooooy211 <alooooy21@gmail.com>
2024-09-08 13:24:46 +0300

    slam

---
+++ Bader/ViewModels/AttendenceViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+using Bader.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bader.ViewModels
+{
+    public class AttendenceViewModel
+    {
+        public int Id { get; set; }
+        public tblSessions Session { get; set; }
+
+        public int SessionId { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime SessionDate { get; set; }
+        public Guid GUID { get; set; } = Guid.NewGuid();
+        public bool IsAttend { get; set; }
+
+    }
+}
