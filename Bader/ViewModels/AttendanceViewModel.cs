0a54ba2d3cd659b619b70aeaafe7684cf14ef9e9
علي بن محمد الغزال <219023289@student.kfu.edu.sa>
2024-09-10 13:30:03 +0300

    attend

--- Bader/ViewModels/AttendanceViewModel.cs
+++ Bader/ViewModels/AttendanceViewModel.cs
@@ -9,6 +9,18 @@ namespace Bader.ViewModels
 {
     public class AttendanceViewModel
     {
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [DisplayName("الاسم")]
+        [StringLength(100)]
+        public string CollegeNameAr { get; set; }
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [DisplayName("Name")]
+        [StringLength(100)]
+        public string CollegeNameEn { get; set; }
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [DisplayName("الرمز")]
+
         public int Id { get; set; }
         public tblSessions Session { get; set; }
 
@@ -19,6 +31,13 @@ namespace Bader.ViewModels
         public DateTime SessionDate { get; set; }
         public Guid GUID { get; set; } = Guid.NewGuid();
         public bool IsAttend { get; set; }
+
+        public string CourseNameAr { get; set; }
+
+        public string SessionNameAr { get; set; }
+
+
+ 
 
     }
 }
