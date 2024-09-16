03e93c8345eeb3491047480d00361e8e05d4e4f8
علي بن محمد الغزال <219023289@student.kfu.edu.sa>
2024-09-16 12:23:34 +0300

    attend

--- Bader/Domain/AttendanceDomain.cs
+++ Bader/Domain/AttendanceDomain.cs
@@ -1,7 +1,87 @@
-﻿using Bader.Models;
+﻿//using Bader.Models;
+//using Bader.ViewModels;
+//using Microsoft.EntityFrameworkCore;
+//using System;
+
+//namespace Bader.Domain
+//{
+//    public class AttendanceDomain
+//    {
+//        private readonly BaaderContext _context;
+
+//        public AttendanceDomain(BaaderContext context)
+//        {
+//            _context = context;
+//        }
+
+//        //Get all attendance records
+//        public async Task<IEnumerable<AttendanceViewModel>> GetAllAttendance()
+//        {
+//            try
+//            {
+//                return await _context.tblAttendance
+//                    .Include(a => a.Session)
+//                    .Select(a => new AttendanceViewModel
+//                    {
+//                        Id = a.Id,
+//                        SessionId = a.SessionId,
+//                        SessionDate = a.SessionDate,
+//                        UserName = a.UserName,
+//                        IsAttend = a.IsAttend,
+//                        GUID = a.GUID
+//                    })
+//                    .ToListAsync();
+//            }
+//            catch
+//            {
+//                return new List<AttendanceViewModel>();
+//            }
+//        }
+
+//        Get attendance by session ID
+//        public async Task<IEnumerable<AttendanceViewModel>> GetAttendanceBySessionId(int sessionId)
+//        {
+//            try
+//            {
+//                return await _context.tblAttendance
+//                    .Where(a => a.SessionId == sessionId)
+//                    .Select(a => new AttendanceViewModel
+//                    {
+//                        Id = a.Id,
+//                        SessionId = a.SessionId,
+//                        SessionDate = a.SessionDate,
+//                        UserName = a.UserName,
+//                        IsAttend = a.IsAttend,
+//                        GUID = a.GUID
+//                    })
+//                    .ToListAsync();
+//            }
+//            catch
+//            {
+//                return new List<AttendanceViewModel>();
+//            }
+//        }
+
+
+
+
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+using Bader.Models;
 using Bader.ViewModels;
 using Microsoft.EntityFrameworkCore;
-using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Bader.Domain
 {
@@ -14,7 +94,7 @@ namespace Bader.Domain
             _context = context;
         }
 
-        //Get all attendance records
+        // Get all attendance records
         public async Task<IEnumerable<AttendanceViewModel>> GetAllAttendance()
         {
             try
@@ -28,7 +108,8 @@ namespace Bader.Domain
                         SessionDate = a.SessionDate,
                         UserName = a.UserName,
                         IsAttend = a.IsAttend,
-                        GUID = a.GUID
+                        GUID = a.GUID,
+                        SessionNameAr = a.Session.SessionNameAr // Ensure this property is included
                     })
                     .ToListAsync();
             }
@@ -38,32 +119,30 @@ namespace Bader.Domain
             }
         }
 
-        // Get attendance by session ID
-        //public async Task<IEnumerable<AttendanceViewModel>> GetAttendanceBySessionId(int sessionId)
-        //{
-        //    try
-        //    {
-        //        return await _context.tblAttendance
-        //            .Where(a => a.SessionId == sessionId)
-        //            .Select(a => new AttendanceViewModel
-        //            {
-        //                Id = a.Id,
-        //                SessionId = a.SessionId,
-        //                SessionDate = a.SessionDate,
-        //                UserName = a.UserName,
-        //                IsAttend = a.IsAttend,
-        //                GUID = a.GUID
-        //            })
-        //            .ToListAsync();
-        //    }
-        //    catch
-        //    {
-        //        return new List<AttendanceViewModel>();
-        //    }
-        //}
-
-
-
-
+        // Get attendance by ID
+        public async Task<AttendanceViewModel> GetAttendanceById(int id)
+        {
+            try
+            {
+                return await _context.tblAttendance
+                    .Include(a => a.Session)
+                    .Where(a => a.Id == id)
+                    .Select(a => new AttendanceViewModel
+                    {
+                        Id = a.Id,
+                        SessionId = a.SessionId,
+                        SessionDate = a.SessionDate,
+                        UserName = a.UserName,
+                        IsAttend = a.IsAttend,
+                        GUID = a.GUID,
+                        SessionNameAr = a.Session.SessionNameAr // Ensure this property is included
+                    })
+                    .FirstOrDefaultAsync();
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
