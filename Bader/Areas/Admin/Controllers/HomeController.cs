d1a2a9c3eacfe3a8e05ca3b1bd1937e712dd39d3
alooooy211 <alooooy21@gmail.com>
2024-09-18 19:33:48 +0300

    slam

--- Bader/Areas/Admin/Controllers/HomeController.cs
+++ Bader/Areas/Admin/Controllers/HomeController.cs
@@ -9,27 +9,95 @@ namespace Bader.Areas.Admin.Controllers
     {
         private readonly CourseDomain _CourseDomain;
         private readonly CollegeDomain _collegeDomain;
+        private readonly UserDomain _userDomain;
+        private readonly PermissionDomain _PermissionDomain;
+        private readonly SessionDomain _sessionDomain;
 
-        public HomeController(CourseDomain context, CollegeDomain collegeDomain)
+        public HomeController(CourseDomain context, CollegeDomain collegeDomain, UserDomain userDomain, PermissionDomain permissionDomain , SessionDomain sessionDomain)
         {
             _CourseDomain = context;
             _collegeDomain = collegeDomain;
+            _userDomain = userDomain;
+            _PermissionDomain = permissionDomain;
+            _sessionDomain = sessionDomain;
         }
         public async Task <IActionResult> Index()
         {
-            var Coursecount = await _CourseDomain.GetAllCourses();
-            ViewBag.CourseCount = Coursecount.Count();
-            var colleges = await _collegeDomain.GetAllColleges();
+            var PerCount = await _PermissionDomain.GetPermissions();
+         
+            ViewBag.AdminCount = 0;
 
-            ViewBag.CourseList = new List<int>();
-            ViewBag.CollegeName = new List<string>();
-
-            foreach (var college in colleges)
+            foreach (var role in PerCount)
             {
-                var coursebycollege = await _CourseDomain.GetCoursesCount(college.CollegeCode);
-                ViewBag.CourseList.Add(coursebycollege);
-                ViewBag.CollegeName.Add(college.CollegeNameAr);
+                if (role.RoleId == 1) {
+                    ViewBag.AdminCount = ViewBag.AdminCount + 1;
+                }
             }
+
+            var UsersCount = await _userDomain.GetUsers();
+            ViewBag.UsersCount = UsersCount.Count();
+
+            var CourseCount = await _CourseDomain.GetAllCourses();
+            ViewBag.CourseCount = CourseCount.Count();
+
+            var SessionCount = await _sessionDomain.GetSessions();
+            ViewBag.SessionCount = SessionCount.Count();
+
+            ViewBag.Open = 0;
+            ViewBag.Close = 0;
+            foreach (var sessionstate in SessionCount) { 
+            
+                if ( sessionstate.StateAr == "متاح")
+                {
+                    ViewBag.Open = ViewBag.Open + 1;
+                }
+                else
+                {
+                    ViewBag.Close = ViewBag.Close + 1;
+                }
+            }
+
+            ViewBag.Session2022 = 0;
+            ViewBag.Session2024 = 0;
+            ViewBag.Session2026 = 0;
+            ViewBag.Session2028 = 0;
+            ViewBag.Session2030 = 0;
+            foreach ( var sessiondate in SessionCount)
+            {
+                if ( sessiondate.SessionDate.Year == 2022 || sessiondate.SessionDate.Year == 2023)
+                {
+                    ViewBag.Session2022 += 1;
+                }
+                else if(sessiondate.SessionDate.Year == 2024 || sessiondate.SessionDate.Year == 2025)
+                {
+                    ViewBag.Session2024 += 1;
+                }
+                else if (sessiondate.SessionDate.Year == 2026 || sessiondate.SessionDate.Year == 2027)
+                {
+                    ViewBag.Session2026 += 1;
+                }
+                else if (sessiondate.SessionDate.Year == 2028 || sessiondate.SessionDate.Year == 2029)
+                {
+                    ViewBag.Session2028 += 1;
+                }
+                else
+                {
+                    ViewBag.Session2030 += 1;
+                }
+            }
+
+            //var colleges = await _collegeDomain.GetAllColleges();
+
+            //ViewBag.CourseList = new List<int>();
+            //ViewBag.CollegeName = new List<string>();
+
+            //foreach (var college in colleges)
+            //{
+            //    var coursebycollege = await _CourseDomain.GetCoursesCount(college.CollegeCode);
+            //    ViewBag.CourseList.Add(coursebycollege);
+            //    ViewBag.CollegeName.Add(college.CollegeNameAr);
+            //}
+
             return View();
         }
         public IActionResult Profile()
